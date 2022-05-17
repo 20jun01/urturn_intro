32dad3dea0495d41e86a8ab99bff4cb9cb23e357
20jun01 <10junjun20@gmail.com>
2022-05-17 22:05:23 +0900

    make counter

--- works/counter.js
+++ works/counter.js
@@ -2,5 +2,11 @@ let count = 0
 const countUp = () => {
   count++
   const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+  countElement.innerText = '回数: ' + count + "\n　"
+}
+
+const reset = () => {
+    count = 0
+    const countElement = document.querySelector('#count')
+    countElement.innerText = '回数: ' + count + '\n 回数はリセットされました'
 }
